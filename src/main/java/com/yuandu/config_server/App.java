564384ab8d3ce8fa8dd1c0220c1e2a874125c660
longyuanbing <longyuanbing@lifesense.com>
2017-09-04 22:31:43 +0800

    zookeeper

--- src/main/java/com/yuandu/config_server/App.java
+++ src/main/java/com/yuandu/config_server/App.java
@@ -2,12 +2,15 @@ package com.yuandu.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
 }
