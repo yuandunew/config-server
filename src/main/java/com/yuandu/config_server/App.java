aa1ba395ee4c32adc92937c506724984b5d570a4
longyuanbing <longyuanbing@lifesense.com>
2017-09-12 10:52:08 +0800

    eureka

--- src/main/java/com/yuandu/config_server/App.java
+++ src/main/java/com/yuandu/config_server/App.java
@@ -3,9 +3,11 @@ package com.yuandu.config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
